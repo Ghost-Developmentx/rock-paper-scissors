481e5a60784200a1d63f21b8a6160c35456d948e
Ghost <ghostdevelopmentx@protonmail.com>
2025-02-19 12:34:59 -0500

    Add final playGame Function, project complete

--- script.js
+++ script.js
@@ -28,3 +28,39 @@ function playRound(humanChoice, computerChoice) {
         return "Computer Wins"
     }
 }
+
+function playGame() {
+    humanScore = 0
+    computerScore = 0
+
+    for (let round = 1; round <= 5; round++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice()
+        let result = playRound(humanChoice, computerChoice)
+
+        if (result === "Human Wins") {
+            humanScore++
+            console.log(`Human Wins Round ${round}! Score: ${humanScore}-${computerScore}`)
+        } else if (result === "Computer Wins") {
+            computerScore++
+            console.log(`Computer Wins Round ${round}! Score: ${humanScore}-${computerScore}`)
+        } else {
+            console.log(`Round ${round} is a tie! Score: ${humanScore}-${computerScore}`)
+        }
+
+        if (round < 5) {
+            console.log(`Get Ready for Round ${round + 1}`)
+        }
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`Human Wins Game! Score: ${humanScore}-${computerScore}`)
+    } else if (computerScore > humanScore) {
+        console.log(`Computer Wins Game! Score: ${humanScore}-${computerScore}`)
+    } else {
+        console.log(`The game is a tie! Score: ${humanScore}-${computerScore}`)
+    }
+    
+}
+
+playGame()
